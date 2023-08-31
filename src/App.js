b9bc790324338bcb72a7430e96a9f877376be8bd
Thiago Amim <thiagoamim11@gmail.com>
2023-08-31 17:10:54 -0300

    testando

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Hoje o Sao paulo vence a LDU
         </p>
         <a
           className="App-link"
