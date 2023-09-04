b671aedda7a50ca92f4caa1b0cc7d0c4bcc37f9e
thiagooterolima <72893583+thiagooterolima@users.noreply.github.com>
2023-09-04 10:55:50 -0300

    tela começada

--- src/App.js
+++ src/App.js
@@ -1,22 +1,109 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
 
 function App() {
+  const options =[
+    {value: '', text: 'Escolha o Sexo', disabled: true},
+    {value:'sexoM' ,text: 'Masculino'},
+    {value:'sexoF' ,text: 'Feminino'},
+  ];
+
+  const optionsV =[
+    {value: '', text: 'Escolha o Veterinario', disabled: true},
+    {value:'V1' ,text: 'Joao'},
+    {value:'V2' ,text: 'Marcia'},
+    {value:'V3' ,text: 'Pedro'},  
+  ];
+
+
+
+  const [selected, setSelected] = useState('');
+
+  const handleChange  = event => {
+    console.log('Label ', event.target.selectedOptions[0].label);
+    console.log(event.target.value);
+
+    setSelected(event.target.value);
+  };
+
+  const [selectedV, setSelectedV] = useState('');
+
+  const handleChangeV  = event => {
+    console.log('Label ', event.target.selectedOptions[0].label);
+    console.log(event.target.value);
+
+    setSelectedV(event.target.value);
+  };
+
+
+
+  const selectStyle = {
+    fontSize: '20px',
+    float : 'left',
+    marginLeft:'12px',
+    marginTop:'12px',
+    width: '225px', 
+
+
+  };
+
+  const optionStyle = {
+    fontSize: '16px', 
+  };
+
+ 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hoje o Sao paulo vence a LDU
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <h className="App-coluna">
+        <h className="App-letra">
+          Paciente
+        </h>
+          <input className="App-container"
+           type="text" placeholder = "Nome Do Animal" />
+
+          <input className="App-container"
+           type="text" placeholder = "Nome Do Proprietario" />
+
+          <input className="App-container"
+           type="number" min = "0" placeholder = "Idade" />
+
+        <select value={selected} onChange={handleChange} style={selectStyle}>
+          {options.map(option => (
+           <option
+             disabled={option.disabled}
+             key={option.value}
+             value={option.value}
+             style={optionStyle}
+
+           >
+          {option.text}
+          </option>
+        ))}
+      </select>
+
+      <select value={selectedV} onChange={handleChangeV} style={selectStyle}>
+          {optionsV.map(option => (
+           <option
+             disabled={option.disabled}
+             key={option.value}
+             value={option.value}
+             style={optionStyle}
+
+           >
+          {option.text}
+          </option>
+        ))}
+      </select>
+
+
+
+
+
+      </h>
+       
       </header>
     </div>
   );
